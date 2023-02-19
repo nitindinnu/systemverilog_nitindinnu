b373a952075fc6153de4c1f439c4f783fcccd604
Nitin Dinnu <68476175+nitindinnu@users.noreply.github.com>
2023-02-19 23:42:33 +0530

    .

---
+++ 00datatypes_sv/03/store_array_in_queue.do
@@ -0,0 +1,4 @@
+vlib work
+vlog store_array_in_queue.sv
+vsim store_array_in_queue -l run.log
+run -all
--- 00datatypes_sv/03/store_array_in_queue.sv
+++ 00datatypes_sv/03/store_array_in_queue.sv
@@ -1,6 +1,20 @@
 module store_array_in_queue(
 );
     /*Ex : `arr[8] = '{5,6,8,3,4,9,7,2}'` is the input sort & store in the queue `Q1[$]` and display that queue*/
-
-    reg[2:0] arr;
+    int arr[8] ='{5,6,8,3,4,9,7,2};
+    int q1[$:7];
+    initial begin
+        $display("%p",arr);
+        arr.sort();
+        $display("arr vlues after sorting %p",arr); //'{2, 3, 4, 5, 6, 7, 8, 9}
+        // q1.insert(arr[0:$]);
+        foreach (arr[i]) begin
+            // q1.push_front(arr[i]);{9, 8, 7, 6, 5, 4, 3, 2}
+            q1.push_back(arr[i]);//{2, 3, 4, 5, 6, 7, 8, 9}
+        end
+        // q1.push_back(arr);//* Error: store_array_in_queue.sv(15): Bit stream casting is only allowed on same size data
+        $display("queue values %p",q1);
+        /*
+        */
+    end
 endmodule
